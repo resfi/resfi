640d2311556c7723a00c54baa65ea85de4b7c80a
ResFi <resfi@tkn.tu-berlin.de>
2016-03-23 10:47:18 +0100

    Update install_deps.sh

--- install_deps.sh
+++ install_deps.sh
@@ -2,24 +2,27 @@
 
 echo "Installing base packages ... "
 sudo apt-get update
-sudo apt-get install git gawk build-essential autoconf libtool pkg-config
+sudo apt-get -y install git gawk build-essential autoconf libtool pkg-config
 
 echo "Installing python-pip, netifaces for mininet ... "
-sudo apt-get install python-dev
-sudo apt-get install python-pip
+sudo apt-get -y install python-dev
+sudo apt-get -y install python-pip
 sudo pip install netifaces
 sudo pip install pycrypto
 
 echo "Installing ZeroMQ"
-sudo apt-get install libzmq3-dev
-sudo apt-get install python-zmq
+sudo apt-get -y install libzmq3-dev
+sudo apt-get -y install python-zmq
 
 echo "Installing libnl-dev for hostapd and iw..."
-sudo apt-get install libnl-3-dev
-sudo apt-get install libnl-genl-3-dev
+sudo apt-get -y install libnl-3-dev
+sudo apt-get -y install libnl-genl-3-dev
 
 echo "##########################################"
-echo "Setup finished, please adjust CONNECTOR and WIRED_INTERFACE in file framework/config.py"
-echo "e.g vim framework/config.py"
-echo "When finished start ResFi Agent with ./start_resfi.sh phyX"
+echo "ResFi setup finished." 
+echo "Please adjust CONNECTOR and WIRED_INTERFACE" 
+echo "in file framework/config.py"
+echo ""
+echo "Start ResFi Agent with:" 
+echo "./start_resfi.sh phyX"
 echo "##########################################"
