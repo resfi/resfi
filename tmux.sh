9a31f5a39b7ffcdf9657f65de3b1664822443268
Sven Zehl <zehl@tkn.tu-berlin.de>
2017-05-29 15:53:32 +0200

    tmux auto session

---
+++ tmux.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+SESSION=$USER
+
+tmux -2 new-session -d -s $SESSION
+
+# Setup a window for tailing log files
+tmux new-window -t $SESSION:1 -n 'ResFi Demo'
+tmux split-window -h
+tmux select-pane -t 0
+tmux send-keys 'ssh 192.168.200.29' C-m
+tmux select-pane -t 1
+tmux send-keys "ssh 192.168.200.10" C-m
+tmux split-window -v
+tmux send-keys "ssh 192.168.200.15" C-m
+tmux select-pane -t 0
+tmux split-window -v
+tmux send-keys "ssh 192.168.200.40" C-m
+
+# Set default window
+tmux select-window -t $SESSION:1
+
+# Attach to session
+tmux -2 attach-session -t $SESSION
