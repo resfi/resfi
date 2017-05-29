da072d3e54350c1645667583e1f5287c0e74e2a6
Sven Zehl <zehl@tkn.tu-berlin.de>
2017-05-29 11:45:59 +0200

    add mailbox.cpp failure recovery!

---
+++ check_mailbox_failure.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+sleep 2
+
+while true; do 
+    if grep -F "mailbox.cpp" /tmp/resfi_console_demo.log
+    then
+        # code if found
+        echo "Error found rebooting ResFi"
+        sudo kill -9 $(ps aux | grep "sudo python resfi_loader.py" | awk '{ print $2 }' | head -n1)
+        sudo kill -9 $(ps aux | grep "python resfi_loader.py" | awk '{ print $2 }' | head -n1)
+        rm /tmp/resfi_console_demo.log
+        sleep 1
+        /home/robat/resfi/start_resfi_only.sh
+    else
+        echo "Check complete, no erros found."
+        # code if not found
+    fi
+    sleep 1
+done
--- start_resfi_only.sh
+++ start_resfi_only.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
+sudo killall -9 check_mailbox_failure.sh
+sleep 1
+/home/robat/resfi/check_mailbox_failure.sh &
+
 cd framework/
 sudo python resfi_loader.py
+
