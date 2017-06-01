8e88e283340d42238ed2685af70aa99865408038
Sven Zehl <zehl@tkn.tu-berlin.de>
2017-06-01 17:07:03 +0200

    improve tmux script

--- tmux.sh
+++ tmux.sh
@@ -8,13 +8,21 @@ tmux new-window -t $SESSION:1 -n 'ResFi Demo'
 tmux split-window -h
 tmux select-pane -t 0
 tmux send-keys 'ssh 192.168.200.29' C-m
+tmux send-keys 'cd resfi' C-m
+tmux send-keys 'watch -n 0,1 "tail -n10 /tmp/resfi_console_demo.log"' C-m
 tmux select-pane -t 1
 tmux send-keys "ssh 192.168.200.10" C-m
+tmux send-keys 'cd resfi' C-m
+tmux send-keys 'watch -n 0,1 "tail -n10 /tmp/resfi_console_demo.log"' C-m
 tmux split-window -v
 tmux send-keys "ssh 192.168.200.15" C-m
+tmux send-keys 'cd resfi' C-m
+tmux send-keys 'watch -n 0,1 "tail -n10 /tmp/resfi_console_demo.log"' C-m
 tmux select-pane -t 0
 tmux split-window -v
 tmux send-keys "ssh 192.168.200.40" C-m
+tmux send-keys 'cd resfi' C-m
+tmux send-keys 'watch -n 0,1 "tail -n10 /tmp/resfi_console_demo.log"' C-m
 
 # Set default window
 tmux select-window -t $SESSION:1
